6a66d64a8aef2306c87be7abcd650abb77743835
donnx32 <32759912+donnx32@users.noreply.github.com>
2018-04-16 15:52:22 +0800

    Fixed a character.

--- src/udc/database/DataBaseController.java
+++ src/udc/database/DataBaseController.java
@@ -222,7 +222,7 @@ public class DataBaseController {
      * @param clientID   — ID of the client
      */
     public void addAppointment(LocalDateTime time_start, LocalDateTime time_end, int doctorID, int clientID) {
-        String stmt = "INSERT INTO clinic_db.appointment (time_start, time_end, doctor_id, client_id) VALUES (?, ?, ?)";
+        String stmt = "INSERT INTO clinic_db.appointment (time_start, time_end, doctor_id, client_id) VALUES (?, ?, ?, ?)";
         try {
             connection = ConnectionConfiguration.getConnection(model);
             pStmt = connection.prepareStatement(stmt);
