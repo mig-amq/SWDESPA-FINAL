455ef13d4c91716f7b82877d9f30bdf374e28e75
prbg <plinkygamara@gmail.com>
2018-04-16 19:36:37 +0800

    updated client views

---
+++ src/udc/client/regular/FXMLControllers/ClientAgendaDayController.java
@@ -0,0 +1,51 @@
+package udc.client.regular.FXMLControllers;
+
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ListView;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ClientAgendaDayController extends ClientSuperController implements Initializable {
+
+    @FXML
+    private ListView dayList;
+
+    private ObservableList<String> items;
+
+
+    private void setList() {
+        items.add("00:00" + "-" + "02:30" + " " + "Dr JDC");
+        items.add("00:00" + "-" + "01:30" + " " + ":)");
+        //dayList.setItems(items);
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        items = dayList.getItems();
+//        setList();
+//        dayList.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<String>() {
+//            @Override
+//            public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
+//                System.out.println("oldValue = " + oldValue + " to newValue = " + newValue);
+//                switch(newValue) {
+//                    case "Monday":
+//                    case "Tuesday":
+//                    case "Wednesday":
+//                    case "Thursday":
+//                    case "Friday":
+//                    case "Saturday":
+//                    case "Sunday":
+//                    default:
+//                }
+//            }
+//        });
+    }
+
+    @Override
+    public void insertFilterData() {
+
+    }
+}
