94196fcbf0ce2b8a6e0dc979b5890cdfbd7190bc
prbg <plinkygamara@gmail.com>
2018-04-18 04:33:48 +0800

    updated reserveController

--- src/udc/client/regular/FXMLControllers/ReserveController.java
+++ src/udc/client/regular/FXMLControllers/ReserveController.java
@@ -9,7 +9,11 @@ import java.io.IOException;
 import java.net.URL;
 import java.time.DayOfWeek;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.ResourceBundle;
@@ -18,14 +22,13 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.control.DateCell;
-import javafx.scene.control.DatePicker;
-import javafx.scene.control.Label;
-import javafx.scene.control.Tooltip;
+import javafx.scene.control.*;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 import javafx.util.Callback;
 import udc.Model;
+import udc.objects.time.concrete.Agenda;
+import udc.objects.time.concrete.Unavailable;
 
 public class ReserveController extends AnchorPane {
 
@@ -37,7 +40,7 @@ public class ReserveController extends AnchorPane {
     @FXML private Label lblAlert;
     @FXML private JFXDatePicker datePicker;
     @FXML private JFXComboBox<String> doctorCmb;
-    @FXML private JFXComboBox<String> startHour, startMin, endHour, endMin;
+    @FXML private JFXComboBox<String> startHour, startMin, endHour, endMin, startM, endM;
     @FXML private Model model;
 
     public ReserveController(Model model) {
@@ -54,15 +57,18 @@ public class ReserveController extends AnchorPane {
     }
 
     public void setComboBox() {
-        ObservableList<String> hourList = FXCollections.observableArrayList();
-        ObservableList<String> minList = FXCollections.observableArrayList("00", "30");
-        ObservableList<String> doctorList = FXCollections.observableArrayList(model.getDbController().loadDoctors());
+        ObservableList<String> hourList, minList, doctorList, mList;
 
-        for (int i = 7; i < 21; i++) {
-            if (i < 10)
-                hourList.add("0" + i);
+        hourList = FXCollections.observableArrayList();
+        minList = FXCollections.observableArrayList("00", "30");
+        doctorList = FXCollections.observableArrayList(model.getDbController().loadDoctors());
+        mList = FXCollections.observableArrayList("AM", "PM");
+
+        for (int i = 1; i < 13; i++) {
+            if(i < 10)
+                hourList.add("0"+i);
             else
-                hourList.add(i + "");
+                hourList.add((""+i));
         }
 
         startHour.setItems(hourList);
@@ -70,18 +76,13 @@ public class ReserveController extends AnchorPane {
         startMin.setItems(minList);
         endMin.setItems(minList);
         doctorCmb.setItems(doctorList);
-
-        startHour.setValue("07");
-        endHour.setValue("08");
-        startMin.setValue("30");
-        endMin.setValue("00");
+        startM.setItems(mList);
+        endM.setItems(mList);
 
         datePicker.setValue(LocalDate.now());
-        datePicker.setShowWeekNumbers(true);
 
         Callback<DatePicker, DateCell> dayCellFactory= this.getDayCellFactory();
         datePicker.setDayCellFactory(dayCellFactory);
-
     }
 
     private Callback<DatePicker, DateCell> getDayCellFactory() {
@@ -106,24 +107,131 @@ public class ReserveController extends AnchorPane {
     }
 
     public void initializeButtons() {
-
         close.setOnMouseClicked(event -> {
             Stage stage = (Stage) close.getScene().getWindow();
             stage.close();
         });
 
         reserve.setOnMouseClicked(event -> {
-            String startTime = startHour.getSelectionModel().getSelectedItem();
-            startTime += ":";
-            startTime += startMin.getSelectionModel().getSelectedItem();
+            if(startHour.getValue()== null || startMin.getValue() == null || startM.getValue() == null ||
+                    endHour.getValue() == null || endMin.getValue() == null|| endM.getValue() == null ||
+                    doctorCmb.getValue() == null) {
+                Alert alert = new Alert (Alert.AlertType.ERROR);
+                alert.setTitle("Invalid Input");
+                alert.setHeaderText(null);
+                alert.setContentText("Please Enter All Info.");
+                alert.showAndWait();
+            }
+            else {
+                LocalDate date = datePicker.getValue();
+                String sTime = startHour.getValue() + ":" + startMin.getValue() + " " + startM.getValue();
+                String eTime = endHour.getValue() + ":" + endMin.getValue() + " " + endM.getValue();
 
-            String endTime = endHour.getSelectionModel().getSelectedItem();
-            endTime += ":";
-            endTime += endMin.getSelectionModel().getSelectedItem();
+                DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy/MM/dd");
+                String startTemp = formatter.format(date) + " " + sTime;
+                String endTemp = formatter.format(date) + " " + eTime;
 
-            System.out.println(startTime + " to " + endTime);
+                LocalDateTime startTime = Agenda.strToTime(startTemp);
+                LocalDateTime endTime = Agenda.strToTime(endTemp);
+
+                //SAME TIME
+                if(startTime.isEqual(endTime)) {
+                    Alert alert = new Alert (Alert.AlertType.ERROR);
+                    alert.setTitle("Invalid Input");
+                    alert.setHeaderText(null);
+                    alert.setContentText("Same start time and end time are not allowed.");
+                    alert.showAndWait();
+                }
+                //GREATER THAN END TIME
+                else if(startTime.isAfter(endTime)) {
+                    Alert alert = new Alert (Alert.AlertType.ERROR);
+                    alert.setTitle("Invalid Input");
+                    alert.setHeaderText(null);
+                    alert.setContentText("Start time is greater than the end time.");
+                    alert.showAndWait();
+                }
+                //LESS THAN OPERATING HOURS
+                else if(startTime.toLocalTime().isBefore(LocalTime.parse("07:30"))) {
+                    Alert alert = new Alert (Alert.AlertType.ERROR);
+                    alert.setTitle("Invalid Input");
+                    alert.setHeaderText(null);
+                    alert.setContentText("Clinic opens at 7:30 AM");
+                    alert.showAndWait();
+                }
+                //DURING OPERATING HOURS
+                else if(startTime.isBefore(LocalDateTime.now())) {
+                    Alert alert = new Alert (Alert.AlertType.ERROR);
+                    alert.setTitle("Invalid Input");
+                    alert.setHeaderText(null);
+                    alert.setContentText("Time has already passed");
+                    alert.showAndWait();
+                }
+                //GREATER THAN OPERATING HOURS
+                else if(startTime.toLocalTime().isAfter(LocalTime.parse("22:30")) ||
+                        endTime.toLocalTime().isAfter(LocalTime.parse("22:00"))) {
+                    Alert alert = new Alert (Alert.AlertType.ERROR);
+                    alert.setTitle("Invalid Input");
+                    alert.setHeaderText(null);
+                    alert.setContentText("Clinic closes at 10:00 PM");
+                    alert.showAndWait();
+                }
+                else {
+                    System.out.println("OKAY!!");
+                    boolean canAdd = true;
+
+
+
+                    while(startTime.isBefore(endTime) || startTime.equals(endTime)) {
+                        if(isOverLap(startTime)) {
+                            Alert alert = new Alert (Alert.AlertType.ERROR);
+                            alert.setTitle("Invalid Input");
+                            alert.setHeaderText(null);
+                            alert.setContentText("Dr. " + doctorCmb.getValue() + "is not available at this time.");
+                            alert.showAndWait();
+                            canAdd = false;
+                            break;
+                        }
+                        if(startTime.equals(endTime)) break;
+                        startTime = startTime.plusMinutes(30);
+                    }
+                    if(canAdd) {
+                        System.out.println("CAN ADD!");
+                    }
+
+                }
+
+
+            }
         });
 
+    }
+
+    private boolean isOverLap(LocalDateTime candidate) {
+        ArrayList<Unavailable> unavailList = null;
+        try {
+            unavailList = model.getDbController().getUnvailability(doctorCmb.getValue());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        System.out.println("doctors " + unavailList.size());
+
+        for(int i = 0; i < unavailList.size(); i++) {
+            Unavailable u = unavailList.get(i);
+
+            if(u.getStartTime().toLocalDate().isEqual(candidate.toLocalDate())) {
+                LocalTime startTime = u.getStartTime().toLocalTime();
+                LocalTime endTime = u.getEndTime().toLocalTime();
+
+                if(isBetween(candidate.toLocalTime(), startTime, endTime)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    private boolean isBetween(LocalTime candidate, LocalTime start, LocalTime end) {
+        return !candidate.isBefore(start) && !candidate.isAfter(end);
     }
 
 }
