511a2e731f84a3969b3b7be44ea14af9c07ec436
gracielnunciob <graciel_nuncio@dlsu.edu.ph>
2018-04-17 08:06:02 +0800

    For Client but currently commented out

--- src/udc/client/regular/FXMLControllers/DoctorAgendaDayController.java
+++ src/udc/client/regular/FXMLControllers/DoctorAgendaDayController.java
@@ -15,8 +15,32 @@ public class DoctorAgendaDayController extends ClientSuperController implements 
     @FXML private ListView<?> dayList;
 
 
+    public void setList()
+    {
+        //        ArrayList<Agenda> today = new ArrayList<Agenda>();
+//
+//
+//        for (int i = 0; i < temp.size(); i++)
+//        {
+//            LocalDateTime timetemp = model.getDbController().getAppointments().get(i).getStartTime();
+//            LocalDateTime now = LocalDateTime.now();
+//
+//            if (timetemp.getDayOfYear() == now.getDayOfYear() && timetemp.getYear() == now.getYear())
+//            {
+//                today.add(model.getDbController().getAppointments().get(i));
+//            }
+//        }
+//
+//        items.add(info);
+//
+    }
+
+
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+
+
 
     }
 
--- src/udc/client/regular/FXMLControllers/DoctorAgendaWeekController.java
+++ src/udc/client/regular/FXMLControllers/DoctorAgendaWeekController.java
@@ -1,5 +1,6 @@
 package udc.client.regular.FXMLControllers;
 
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ListView;
@@ -14,6 +15,14 @@ public class DoctorAgendaWeekController extends ClientSuperController implements
 
     @FXML private ListView<?> weekList;
 
+    private ObservableList<Agenda> mon;
+    private ObservableList<Agenda> tues;
+    private ObservableList<Agenda> wed;
+    private ObservableList<Agenda> thurs;
+    private ObservableList<Agenda> fri;
+    private ObservableList<Agenda> sat;
+    private ObservableList<Agenda> sun;
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
@@ -21,6 +30,21 @@ public class DoctorAgendaWeekController extends ClientSuperController implements
 
     @Override
     public void insertFilterData(ArrayList<Agenda> data) {
-
-    }
+//        for (int i = 0; i < data.size(); i++) {
+//            if (data.get(i).getStartTime().getDayOfWeek().getValue() == 1)
+//                mon.add(data.get(i));
+//            else if (data.get(i).getStartTime().getDayOfWeek().getValue() == 2)
+//                tues.add(data.get(i));
+//            else if (data.get(i).getStartTime().getDayOfWeek().getValue() == 3)
+//                wed.add(data.get(i));
+//            else if (data.get(i).getStartTime().getDayOfWeek().getValue() == 4)
+//                thurs.add(data.get(i));
+//            else if (data.get(i).getStartTime().getDayOfWeek().getValue() == 5)
+//                fri.add(data.get(i));
+//            else if (data.get(i).getStartTime().getDayOfWeek().getValue() == 6)
+//                sat.add(data.get(i));
+//            else if(data.get(i).getStartTime().getDayOfWeek().getValue() == 7)
+//                sun.add(data.get(i));
+//        }
+   }
 }
